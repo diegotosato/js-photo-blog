30aaf106bef95af88d25968155de468bc313eee6
diegotosato <tosato1998@gmail.com>
2025-10-27 11:28:21 +0100

    create AJAX call, isolate row element by class, dceclare function to create card markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,38 @@
 console.log('Photo Blog');
+
+
+/*
+PSEUDO CODE
+
+AJAX call to recover API datas for the card
+isolate the row to create after the cards
+create function to create the html code of the card
+    -card image url refer to object url
+    -card title refer to object title
+    -card text refer to object date
+create loop to print the card in the page
+*/
+
+const endpoint = 'https://lanciweb.github.io/demo/api/pictures/'
+
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+const rowEl = document.getElementsByClassName('row')
+console.log(rowEl);
+
+function createMarkup(object) {
+    return `
+            <div class="card">
+                <img class="pin" src="./assets/img/pin.svg" alt="pin">
+                <img class="card-image" src="${object.url}" alt="img">
+                    <h5 class="card-title">${object.title}</h5>
+                    <p class="card-text">
+                        ${object.date}
+                    </p>
+            </div>
+        `
+}
